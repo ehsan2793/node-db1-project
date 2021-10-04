09abbdd6e5ee716276c65e2264badd3b5889f069
ehsan2793 <ehsan2793@gmail.com>
2021-10-04 16:49:53 -0700

    all test passing

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,18 +1,47 @@
-const { getById } = require('./accounts-model');
-
+const { getById, getAll } = require('./accounts-model');
 
 const checkAccountPayload = async (req, res, next) => {
-  next()
+
+  const { name, budget } = req.body;
+  if (name === undefined || budget === undefined) {
+    next({ status: 400, message: 'name and budget are required' })
+  } else if (typeof name !== 'string') {
+    next({ status: 400, message: "name of account must be a string" })
+  } else if (name.trim().length < 3 || name.trim().length > 100) {
+    next({ status: 400, message: "name of account must be between 3 and 100" })
+  } else if (typeof budget !== 'number') {
+    next({ status: 400, message: "budget of account must be a number" })
+  } else if (budget < 0 || budget > 1000000) {
+
+    next({ status: 400, message: "budget of account is too large or too small" })
+  } else {
+    req.body = { name: name.trim(), budget: budget }
+    next()
+  }
+
 }
-const checkAccountNameUnique = (req, res, next) => {
-  next()
-};
+
+const checkAccountNameUnique = async (req, res, next) => {
+  try {
+
+    const allAccounts = await getAll()
+    allAccounts.find(account => {
+      if (account.name === req.body.name) {
+        next({ status: 400, message: "that name is taken" })
+      }
+    })
+    next()
+  } catch (error) {
+    next(error)
+  }
+
+}
 
 const checkAccountId = async (req, res, next) => {
   const found = await getById(req.params.id);
   try {
     if (!found) {
-      res.status(404).json({ message: 'Account not found' });
+      next({ status: 404, message: 'Account not found' });
     } else {
       req.found = found;
       next();
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,37 +1,57 @@
-const router = require('express').Router()
+const router = require('express').Router();
 const Module = require('./accounts-model');
-const { checkAccountId, checkAccountNameUnique, checkAccountPayload } = require('./accounts-middleware') //eslint-disable-line
+const {
+  checkAccountId,
+  checkAccountNameUnique,
+  checkAccountPayload,
+} = require('./accounts-middleware');
+
 router.get('/', async (req, res, next) => {
   try {
-    const accounts = await Module.getAll()
-    res.status(200).json(accounts)
+    const accounts = await Module.getAll();
+    res.status(200).json(accounts);
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
 router.get('/:id', checkAccountId, async (req, res) => {
-  res.status(200).json(req.found)
-})
-
-
-router.post('/', checkAccountPayload, checkAccountNameUnique, async (req, res, next) => {
-  const newAccount = await Module.create(req.body)
-  res.status(201).json(newAccount)
-})
-
-
-//eslint-disable-next-line
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  res.status(200).json(req.found);
 });
-//eslint-disable-next-line
-router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
 
-router.use((err, req, res, next) => { // eslint-disable-line
-  // DO YOUR MAGIC
-})
+router.post(
+  '/',
+  checkAccountPayload,
+  checkAccountNameUnique,
+  async (req, res) => {
+    console.log(req.body);
+    const newAccount = await Module.create(req.body);
+
+    res.status(201).json(newAccount);
+
+  }
+);
+
+//eslint-disable-next-line
+router.put(
+  '/:id',
+  checkAccountId,
+  checkAccountNameUnique,
+  checkAccountPayload,
+  async (req, res) => {
+    const updatedAccount = await Module.updateById(req.params.id, req.body);
+    res.status(200).json(updatedAccount);
+  },
+);
+//eslint-disable-next-line
+router.delete('/:id', checkAccountId, async (req, res, next) => {
+  const deletedAccount = await Module.deleteById(req.params.id)
+  res.status(200).json(deletedAccount);
+});
+
+router.use((err, req, res, next) => {
+  // eslint-disable-line
+  res.status(err.status || 500).json({ message: err.message });
+});
 
 module.exports = router;
